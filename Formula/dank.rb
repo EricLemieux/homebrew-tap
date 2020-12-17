d20785b51c1820b4607ed187e78eab742a9f00bf
Eric Lemieux <eric@lemieuxdev.com>
2020-12-17 18:21:49 -0500

    Update dank to v1.4.1

--- Formula/dank.rb
+++ Formula/dank.rb
@@ -1,8 +1,8 @@
 class Dank < Formula
   desc "Download memes from reddit in parallel so that you can more efficiently waste time."
   homepage "https://github.com/EricLemieux/dank"
-  url "https://github.com/EricLemieux/dank/archive/v1.4.0.tar.gz"
-  sha256 "83d54f6f6cdf321a74fa00ccdeebead1bf271cb7f0bdbc569231f430394c2024"
+  url "https://github.com/EricLemieux/dank/archive/v1.4.1.tar.gz"
+  sha256 "0258903683864909d96e1ce3f74483639064bd459df0469f6baf102dda277457"
   license "Apache-2.0"
 
   def install
