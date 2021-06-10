13dbe0be89d6456db100612970056f4c6928a1dd
Eric Lemieux <eric@lemieuxdev.com>
2021-06-09 20:08:02 -0400

    Fix dank url

--- Formula/dank.rb
+++ Formula/dank.rb
@@ -2,7 +2,7 @@ class Dank < Formula
   desc "Download memes from reddit in parallel so that you can more efficiently waste time."
   homepage "https://github.com/EricLemieux/dank"
   version "v2.2.0"
-  url "https://github.com/EricLemieux/dank/archive/${version}.tar.gz"
+  url "https://github.com/EricLemieux/dank/releases/download/#{version}/dank-macos.tar.gz"
   sha256 "faa642e629d4f5c1904cd49e9da65e72bc70abcd4e74cbeb31845ecd346423e9"
   license "Apache-2.0"
 
