433a700268c59f9a2103da90a2be14f09bd0cad5
Eric Lemieux <eric@lemieuxdev.com>
2021-09-23 20:57:06 -0400

    Update dank to v2.2.2

--- Formula/dank.rb
+++ Formula/dank.rb
@@ -1,9 +1,9 @@
 class Dank < Formula
   desc "Download memes from reddit in parallel so that you can more efficiently waste time."
   homepage "https://github.com/EricLemieux/dank"
-  version "v2.2.0"
-  url "https://github.com/EricLemieux/dank/releases/download/#{version}/dank-macos.tar.gz"
-  sha256 "f6d3e91242071de18907a459190c1495934f79a674a2943a6d19e02eeea07b72"
+  version "v2.2.2"
+  url "https://github.com/EricLemieux/dank/releases/download/#{version}/dank-#{version}-x86_64-macos.tar.gz"
+  sha256 "bf1a4eb4b7977757ab4f4713be5ffa158630ae2685ca80de9f694e4c47226932"
   license "Apache-2.0"
 
   def install
